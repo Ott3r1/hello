71bc51979fa36e347d4f5d303035341b4c6e16ed
ott3r1 <brandon9ott@gmail.com>
2025-08-28 13:07:30 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+* Author: Brandon Ott
+ * Date: 8/26/2025
+ *
+ * will print my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Brandon Ott!\n");
+
+    return 0;
+}
